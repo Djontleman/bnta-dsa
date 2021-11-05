cc9b57ce2e2452afe151b1a9b1f737fc3d0fa700
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-05 12:55:53 +0000

    Complete day 1 Q3

--- src/main/java/com/djontleman/day1/Main.java
+++ src/main/java/com/djontleman/day1/Main.java
@@ -5,9 +5,14 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(GenerateArray(3)));
-        System.out.println(GenerateKey(3));
+        int testNumber = 25;
 
+        int[] arrayOfNumbers = GenerateArray(testNumber);
+        int key = GenerateKey(testNumber);
+        System.out.println(Arrays.toString(arrayOfNumbers));
+        System.out.println(key);
+
+        System.out.println(LinearSearch(arrayOfNumbers, key));
     }
 
     //  || ======== Part 1 ======== ||
@@ -21,6 +26,24 @@ public class Main {
     //  || ======== Part 2 ======== ||
     public static int GenerateKey(int N) {
         return (int)(Math.random() * N) + 1;
+    }
+
+    //  || ======== Part 3 ======== ||
+    public static int LinearSearch(int[] arr, int key) {
+        if (key < arr[0] || key > arr[arr.length - 1]) {
+            System.out.println("Key outside of array");
+            return 0;
+        } else {
+            int numberOfSteps = 0;
+
+            for (int num : arr) {
+                numberOfSteps++;
+                if (num == key) {
+                    break;
+                }
+            }
+            return numberOfSteps;
+        }
     }
 }
 
