bedf79ac0129084630654da880593f0139a37b3f
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-05 12:39:19 +0000

    Complete day 1 Q2

--- src/main/java/com/djontleman/day1/Main.java
+++ src/main/java/com/djontleman/day1/Main.java
@@ -6,13 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(GenerateArray(3)));
+        System.out.println(GenerateKey(3));
+
     }
 
+    //  || ======== Part 1 ======== ||
     public static int[] GenerateArray(int N) {
         int[] arrayOfNumbers = new int[N];
         for (int i = 0; i < N; i++)
             arrayOfNumbers[i] = i + 1;
         return arrayOfNumbers;
+    }
+
+    //  || ======== Part 2 ======== ||
+    public static int GenerateKey(int N) {
+        return (int)(Math.random() * N) + 1;
     }
 }
 
