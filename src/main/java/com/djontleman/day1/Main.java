821758a909b82561b1476738e63ffe677ec9d659
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-05 14:37:05 +0000

    Complete day 1 extension jump search Reformatted output to be more readable

--- src/main/java/com/djontleman/day1/Main.java
+++ src/main/java/com/djontleman/day1/Main.java
@@ -9,10 +9,12 @@ public class Main {
 
         int[] arrayOfNumbers = GenerateArray(testNumber);
         int key = GenerateKey(testNumber);
-        System.out.println(Arrays.toString(arrayOfNumbers));
-        System.out.println(key);
-
-        System.out.println(LinearSearch(arrayOfNumbers, key));
+        System.out.println("Array: " + Arrays.toString(arrayOfNumbers));
+        System.out.println("Key: " + key);
+        System.out.println();
+        System.out.println("Number of steps");
+        System.out.println("Linear Search: " + LinearSearch(arrayOfNumbers, key));
+        System.out.println("Jump Search: " + JumpSearch(arrayOfNumbers, key, 3));
     }
 
     //  || ======== Part 1 ======== ||
@@ -44,6 +46,50 @@ public class Main {
             }
             return numberOfSteps;
         }
+    }
+
+    //  || ======== Extension ======== ||
+    public static int JumpSearch(int[] arr, int key, int jump) {
+        int numberOfSteps = 0;
+
+        int previousIndex = -1;
+        int previousNumber;
+
+        int currentIndex = 0;
+        int currentNumber;
+
+        numberOfSteps++;
+
+        currentNumber = arr[currentIndex];
+        if (currentNumber == key) {
+            return numberOfSteps;
+        }
+
+        while (currentNumber < key) {
+            numberOfSteps++;
+
+            previousIndex = currentIndex;
+            if (currentIndex + jump >= arr.length) {
+                currentIndex = arr.length - 1;
+            } else {
+                currentIndex += jump;
+            }
+
+            currentNumber = arr[currentIndex];
+            if (currentNumber == key) {
+                return numberOfSteps;
+            }
+        }
+
+        for (int i = previousIndex + 1; i < currentIndex; i++) {
+            numberOfSteps++;
+
+            currentNumber = arr[i];
+            if (currentNumber == key) {
+                break;
+            }
+        }
+        return numberOfSteps;
     }
 }
 
